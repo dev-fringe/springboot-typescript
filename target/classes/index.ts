86d2f50360dc8ba5a3da66dfaf26c1b2b66e1ec7
hdlee <dev.fringe@gmail.com>
2022-10-02 23:28:08 +0900

    init

---
+++ target/classes/index.ts
@@ -0,0 +1,37 @@
+import * as $ from 'jquery';
+
+$('.box').css({
+    backgroundColor : "green",
+    width : "100px",
+    height : "100px"
+});
+
+
+interface Hewan {
+    myInfo () : void;
+}
+
+class Kucing implements Hewan{
+    private _habitat : string;
+
+
+    public set habitat(_habitat : string){
+        this._habitat = _habitat;
+    }
+    public get habitat () : string {
+        return this._habitat;
+    }
+    myInfo(): void {
+        console.log(
+            `
+                Habitat : ${this.habitat}
+            `
+        )
+    }
+    
+}
+
+let kucing = new Kucing();
+kucing.habitat = "KKKK1111";
+
+kucing.myInfo();
